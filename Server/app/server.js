c073ab1448af71769d5cabe0127448478374456f
viniciusfaitao <vfaitaop@gmail.com>
2021-12-02 14:42:03 -0300

    Ajustes finais

--- Server/app/server.js
+++ Server/app/server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 port = 3100
 
 app.listen(port, () => {
-  console.log(`Server started at port ${port}`)
+  console.log(`Server started at http://localhost:${port}`)
 })
